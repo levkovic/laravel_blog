7b0692781450bad785299a5747191538f1d5fa04
Illia <jundev2@gmail.com>
2016-10-25 18:12:38 +0300

    25.1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,15 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+//Auth
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+//Reg
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+
 Route::get('blog/{slug}', ['as' =>'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 
 Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' =>  'blog.index']);
